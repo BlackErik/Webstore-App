9af2ab37e0feb3c1824bf9248638f71229f7e757
BlackErik <blac.erik@gmail.com>
2022-06-27 12:14:14 -0600

    cart showing correctly

--- app.js
+++ app.js
@@ -23,6 +23,27 @@ Vue.component("product", {
   props: ["item", "cart"],
 });
 
+Vue.component("productincart", {
+  template: `
+        <div>
+            <span class="title-tag">{{item.title}}</span>
+            <img class="item-image" v-bind:src="item.image">
+            <span class ="price-tag"> {{item.price}}</span>
+        </div>
+    `,
+  data: function () {
+    return {};
+  },
+  methods: {
+    addToCart: function () {
+      if (this.cart.includes(this.item) == false) {
+        this.cart.push(this.item);
+      }
+    },
+  },
+  props: ["item", "cart"],
+});
+
 var app = new Vue({
   el: "#app",
   data: {
